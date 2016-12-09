f8b257222c5784b87cdc7be30e7af3587dfea6a9
jmuth <joachim.h.muth@gmail.com>
2016-12-09 13:31:33 +0100

    some helper functions

--- blender/helpers.py
+++ blender/helpers.py
@@ -12,7 +12,6 @@ Main helpers functions
 HERE TO INSERT METHOD SUCH AS LOAD_DATA, WRITE_PREDICTION, ....
 """
 
-
 import pandas as pd
 import numpy as np
 import os
@@ -25,6 +24,29 @@ from pyspark.sql import *
 
 def load_csv(filename='../data/data_train.csv'):
     df = pd.read_csv(filename)
+    df['UserID'] = df['Id'].apply(lambda x: int(x.split('_')[0][1:]))
+    df['MovieID'] = df['Id'].apply(lambda x: int(x.split('_')[1][1:]))
+    df['Rating'] = df['Prediction']
+    df = df.drop(['Id', 'Prediction'], axis=1)
+    return df
+
+
+def submission_table(original_df, col_userID, col_movie, col_rate):
+    def id(row):
+        return 'r' + str(int(row[col_userID])) + '_c' + str(int(row[col_movie]))
+
+    def pred(row):
+        return row[col_rate]
+
+    df = pd.DataFrame.copy(original_df)
+    df['Id'] = df.apply(id, axis=1)
+    df['Prediction'] = df.apply(pred, axis=1)
+
+    return df[['Id', 'Prediction']]
+
+
+def extract_from_original_table(original_df):
+    df = pd.DataFrame.copy(original_df)
     df['UserID'] = df['Id'].apply(lambda x: int(x.split('_')[0][1:]))
     df['MovieID'] = df['Id'].apply(lambda x: int(x.split('_')[1][1:]))
     df['Rating'] = df['Prediction']
