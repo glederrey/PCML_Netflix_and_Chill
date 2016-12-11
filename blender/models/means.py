69a2c34578341957227c621a8c4d49af894e4c1d
glederrey <gael.lederrey@epfl.ch>
2016-12-11 12:34:36 +0100

    [UPD] Working again on PCML. New models added

--- blender/models/means.py
+++ blender/models/means.py
@@ -10,9 +10,8 @@ Mean prediction method, assigning movie/user/global mean to all items
 """
 
 import numpy as np
-from helpers_scipy import *
+from helpers import *
 import pandas as pd
-
 
 def user_mean(train, test):
     print("[USER_MEAN] applying")
@@ -79,3 +78,28 @@ def global_mean(train, test):
 
     print("[GLOBAL_MEAN] done")
     return predictions
+    
+def movie_mean_deviation_user(train, test):
+    print("[MOVIE_MEAN_DEVIATION_USER] applying")
+    
+    predictions = pd.DataFrame.copy(test)
+    predictions.Rating = predictions.Rating.apply(lambda x: float(x)) 
+    means = train.groupby('Movie').mean()['Rating']
+    deviation = pd.read_csv('../data/deviations_per_users.csv')
+    
+    def line(df):
+        df['Rating'] = means.loc[int(df['Movie'])] + deviation.loc[int(df['User'])].dev 
+        return df#[['User', 'Movie', 'Rating']]
+        
+    predictions = predictions.apply(line, axis=1)
+    
+    #predictions['Rating'] = np.where(predictions['Rating'] > 5, predictions['Rating'], 5)
+    #predictions['Rating'] = np.where(predictions['Rating'] < 1, predictions['Rating'], 1)
+    
+    predictions['User'] = predictions['User'].astype(int)
+    predictions['Movie'] = predictions['Movie'].astype(int)
+
+    print("[MOVIE_MEAN_DEVIATION_USER] done")
+    return predictions    
+
+    
---
+++ blender/models/medians.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Joachim Muth <joachim.henri.muth@gmail.com>
+#
+# Distributed under terms of the MIT license.
+"""
+median prediction method, assigning movie/user/global median to all items
+"""
+
+import numpy as np
+from helpers import *
+import pandas as pd
+
+def user_median(train, test):
+    print("[USER_MEDIAN] applying")
+    predictions = pd.DataFrame.copy(test)
+    predictions.Rating = predictions.Rating.apply(lambda x: float(x)) 
+    medians = train.groupby('User').median()['Rating']
+
+    def line(df):
+        df['Rating'] = medians.loc[df['User'].iloc[0]]
+        return df#[['User', 'Movie', 'Rating']]
+
+    predictions = predictions.groupby('User').apply(line)
+    #predictions = predictions.apply(line, axis=1)
+
+    # integer for id
+    predictions['User'] = predictions['User'].astype(int)
+    predictions['Movie'] = predictions['Movie'].astype(int)
+
+    print("[USER_MEDIAN] done")
+    return predictions
+
+
+def movie_median(train, test):
+    print("[MOVIE_MEDIAN] applying")
+
+    predictions = pd.DataFrame.copy(test)
+    predictions.Rating = predictions.Rating.apply(lambda x: float(x)) 
+    medians = train.groupby('Movie').median()['Rating']
+
+    def line(df):
+        df['Rating'] = medians.loc[df['Movie'].iloc[0]]
+        return df#[['User', 'Movie', 'Rating']]
+
+    predictions = predictions.groupby('Movie').apply(line)
+    #predictions = predictions.apply(line, axis=1)
+
+    # integer for id
+    predictions['User'] = predictions['User'].astype(int)
+    predictions['Movie'] = predictions['Movie'].astype(int)
+
+    print("[MOVIE_MEDIAN] done")
+    return predictions
+
+
+def global_median(train, test):
+    print("[GLOBAL_MEDIAN] applying")
+
+    predictions = pd.DataFrame.copy(test)
+    predictions.Rating = predictions.Rating.apply(lambda x: float(x)) 
+
+    median = train['Rating'].median()
+       
+    #def line(df):
+    #    df['Rating'] = median
+    #    return df[['User', 'Movie', 'Rating']]
+
+    #predictions = predictions.apply(line, axis=1)
+    
+    predictions.Rating=median
+    
+    # integer for id
+    predictions['User'] = predictions['User'].astype(int)
+    predictions['Movie'] = predictions['Movie'].astype(int)
+
+    print("[GLOBAL_MEDIAN] done")
+    return predictions
+    
+def movie_median_deviation_user(train, test):
+    print("[MOVIE_MEDIAN_DEVIATION_USER] applying")
+    
+    predictions = pd.DataFrame.copy(test)
+    predictions.Rating = predictions.Rating.apply(lambda x: float(x)) 
+    medians = train.groupby('Movie').median()['Rating']
+    deviation = pd.read_csv('../data/deviations_per_users.csv')
+    
+    def line(df):
+        df['Rating'] = medians.loc[int(df['Movie'])] + deviation.loc[int(df['User'])].dev 
+        return df#[['User', 'Movie', 'Rating']]
+        
+    predictions = predictions.apply(line, axis=1)
+    
+    #predictions['Rating'] = np.where(predictions['Rating'] > 5, predictions['Rating'], 5)
+    #predictions['Rating'] = np.where(predictions['Rating'] < 1, predictions['Rating'], 1)
+    
+    predictions['User'] = predictions['User'].astype(int)
+    predictions['Movie'] = predictions['Movie'].astype(int)
+
+    print("[MOVIE_MEDIAN_DEVIATION_USER] done")
+    return predictions    
+
+    
