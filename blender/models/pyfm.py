4e3aed6f9f9e889d059b4ae80aaca527a8688fd4
glederrey <gael.lederrey@epfl.ch>
2016-12-16 14:15:53 +0100

    [ADD] pyfm + [DEL] graphlab data

---
+++ blender/models/pyfm.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction import DictVectorizer
+from pyfm import pylibfm
+
+
+def pyfm(train, test, **arg):
+    print('[PYFM] applying')
+
+    # Get the args
+    num_factors = arg['num_factors']
+    num_iter = arg['num_iter']    
+    task = arg['task']
+    initial_learning_rate = arg['initial_learning_rate']
+    learning_rate_schedule = arg['learning_rate_schedule']
+    
+    (train_data, y_train, train_users, train_items) = prepare_data(train)
+    (test_data, y_test, test_users, test_items) = prepare_data(test)
+    
+    v = DictVectorizer()
+    X_train = v.fit_transform(train_data)
+    X_test = v.transform(test_data)
+    
+    fm = pylibfm.FM(num_factors=num_factors, num_iter=num_iter, task=task, initial_learning_rate=initial_learning_rate, learning_rate_schedule=learning_rate_schedule)
+    
+    fm.fit(X_train,y_train)
+    
+    preds = fm.predict(X_test)
+    
+    for i in range(len(preds)):
+        if preds[i] > 5:
+            preds[i] = 5
+        elif preds[i] < 1:
+            preds[i] = 1
+    
+    df_return = test.copy()
+    
+    df_return.Rating = preds
+    
+    print('[PYFM] done')
+    
+    return df_return
+    
+
+def prepare_data(df):
+    data = []
+    y = list(df.Rating)
+    users=set(df.User.unique())
+    items=set(df.Movie.unique())
+    usrs = list(df.User)
+    movies = list(df.Movie)
+    for i in range(len(df)):
+        y[i] = float(y[i])
+        data.append({ "user_id": str(usrs[i]), "movie_id": str(movies[i])})
+    return (data, np.array(y), users, items)
